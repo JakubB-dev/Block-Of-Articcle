9935872608e4f7cfd44a2bfae7950ec8b4d6c419
kuba <projektbronowicki@gmail.com>
2021-09-04 19:03:32 +0200

    Initial commit

---
+++ articleBlock.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/main.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Logo</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Top</a>
+            <a href="">Add</a>
+        </nav>
+        <main>
+            <h3>Latest added</h3>
+            <section class="quizzesBox">
+                <div class="quizBox">
+                    <img src="img/min.jpg" class="thumbnail"><br>
+                    <div class="text">
+                        <a href="" class="quizLink"><h3 class="quizTitle">Why are cats so bored?</h3></a>
+                        <small class="authorAndTime">User123<br>14-07-2021 21:16</small>
+                        <p class="quizDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis nunc justo, quis sollicitudin dui iaculis vel. Ut felis nisi, pharetra porttitor imperdiet vitae, porttitor at nisi. Duis ultrices dignissim quam, eget tristique lorem posuere eget. Ut vel imperdiet elit. Sed pharetra odio eget erat congue, in interdum dolor malesuada. Vivamus ac nunc tempor, aliquam ante non, venenatis velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
+                        <!--<h3 class="rating">Rating: 3,7/10</h3>--><br>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </body>
+</html>
